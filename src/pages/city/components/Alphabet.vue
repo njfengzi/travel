b6d6918036c2967395f2515c0e8781b3ceb2eae7
njfengzi <719289516@qq.com>
2019-06-26 22:15:38 +0800

    finish city css

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul class="list">
+      <li class='item'>A</li>
+      <li class='item'>A</li>
+      <li class='item'>A</li>
+      <li class='item'>A</li>
+      <li class='item'>A</li>
+      <li class='item'>A</li>
+      <li class='item'>A</li>
+      <li class='item'>A</li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    name: "CityAlphabet"
+  }
+</script>
+
+<style lang='stylus' scoped>
+  @import "../../../assets/styles/varibles.styl"
+  .list
+    position:absolute
+    top:1.58rem
+    bottom:0
+    right:0
+    width:.4rem
+    display:flex
+    flex-direction:column
+    justify-content:center
+    .item
+      text-align:center
+      line-height:.4rem
+      color:$bgColor
+</style>
